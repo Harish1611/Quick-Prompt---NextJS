c6c8b4a3f60a9e630329e9e0fa673c93631b4540
Harish1611 <harishnaidub005@gmail.com>
2023-12-14 18:13:24 +0530

    New: Created Hero Section for Application & Added component file structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
-
 const Home = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      {/* Hero Section */}
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center"> Smart Prompt is an open-source AI prompting tool for modern world to discover, create & share creative prompts</p>
 
-export default Home
+      {/* Feed  */}
+    </section>
+  );
+};
+
+export default Home;
